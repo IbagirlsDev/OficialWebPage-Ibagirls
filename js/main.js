800f783f11b19c72a4f9f07c2c1f5b7585b03f9f
stefaniahe <herrerastefania904@gmail.com>
2021-06-24 14:41:16 -0500

    nosotros y redes
    pagina de nosotros y redes

--- js/main.js
+++ js/main.js
@@ -6,13 +6,7 @@
     
 })(jQuery);
 
-const redes = document.getElementById("redess")
-const boton_lo = document.getElementById("foto_log")
 
-boton_lo.addEventListener("click", (e)=>{
-    e.preventDefault();
-    redes.style.display="block"
-})
 
 
 /* Animación Misión y Visión */
